ff789c509d24d42ea1a92e9545bc7d967367d957
knjoroge3 <ramadhanubuntu@gmail.com>
2024-08-20 20:05:33 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-\
+const char message = "and that piece of art is useful\" - Dora Korpar, 2015-\
 10-19";
 write(STDOUT_FILENO, message, 59);
 
