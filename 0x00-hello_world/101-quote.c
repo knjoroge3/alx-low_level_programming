90dc3eef80551c856dd6428b7e70ac2b518a767c
knjoroge3 <ramadhanubuntu@gmail.com>
2024-08-20 19:53:12 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/**
+*Main -Entry point
+*
+*Return: Always (0) Success
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write (STDOUT_FILENO, message, 43);
+
+return (1);
+} 
