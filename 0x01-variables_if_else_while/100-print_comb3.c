8142fbeb64a38ebef0a0bf2eccd9d304c289eeed
knjoroge3 <ramadhanubuntu@gmail.com>
2024-08-30 11:00:37 +0300

    Inventing is a combination of brains and materials. the more brains yo use, the les materials you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+int a = 0;
+int b;
+
+while (a <= 8)
+{
+b = a + 1;
+while (b <= 9)
+{
+putchar(a + '0');
+putchar(b + '0');
+
+if (a != 8 || b != 9)
+{
+putchar(',');
+putchar(' ');
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+
+return (0);
+}
+
