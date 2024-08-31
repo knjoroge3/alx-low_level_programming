309aff27e1e0900da4cba43152e3d1cdc3433a5c
knjoroge3 <ramadhanubuntu@gmail.com>
2024-08-31 08:52:56 +0300

    The success combination in business is Do what you do better and do more more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+int a = 0, b, c;
+
+while (a <= 8)
+{
+b = a + 1;
+
+while (b <= 9)
+{
+c = b + 1;
+
+while (c <= 9)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+putchar(',');
+putchar(' ');
+
+c++;
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+
+return (0);
+}
+}
