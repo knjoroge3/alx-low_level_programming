054f1b249069701aed6a715feba9dc050280d19d
knjoroge3 <ramadhanubuntu@gmail.com>
2024-09-06 22:08:04 +0300

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,17 @@ int main(void)
 int i = 0;
 int j;
 
-while (i < 99)
+while (i <= 99)
 {
 j = i + 1;
-while (j < 99)
+while (j <= 99)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-if (i != 98 || j != 99)
+if (i != 98 || j != 99) 
 {
 putchar(',');
 putchar(' ');
