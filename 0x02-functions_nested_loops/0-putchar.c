08a2abc2f4ee2c0ad681725e0172bc1ece14580f
knjoroge3 <ramadhanubuntu@gmail.com>
2024-09-14 13:58:03 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/**
+*main - Entry point
+*char: _putchar
+*
+*Return: Always 0 (Success)
+*/
+
+#include <stdio.h>
+#include "main.h"
+
+int _putchar(char);
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
