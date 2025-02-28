179edd7e83cc1c2d3a4a6b61761f621ceb9ac53b
knjoroge3 <ramadhanubuntu@gmail.com>
2025-02-28 15:10:40 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0
+*
+*programme that prints all single digits of base 10
+*
+*/
+
+int main(void)
+{
+int a = 0;
+
+while (a <= 9)
+{
+printf("%d", a);
+a++;
+}
+putchar('\n');
+
+return (0);
+}
