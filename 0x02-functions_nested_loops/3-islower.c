f769fe16d97f9f41a28098c2ce3f6875789acba5
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-01 11:51:31 +0300

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_islower - check for lowercase character
+*
+*@c: Character defination of the argument
+*
+*Return: returns 1 if c is lowercase otherwise it returns 0
+*
+*/
+
+int _islower(int c)
+{
+int a = 97;
+
+if (a == 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
