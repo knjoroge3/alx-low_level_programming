deb69df744d7666414de49ed705358c7446dff34
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-01 21:20:09 +0300

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_abs - Finds an absolute value of an integer
+*
+*Description: Function that prints absolute value
+*
+*Return: An integer value
+*
+*/
+
+int _abs(int)
+{
+int a = 0;
+
+if (a => 0)
+{
+_putchar(a);
+}
+else
+{
+_putchar((a *  -1);
+}
+
+return (0);
+}
