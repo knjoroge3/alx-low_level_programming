51b49fbfbd018e00cef3761ae57132355a969f3f
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-02 22:23:20 +0300

    I am federal agent jack bauer and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,21 +10,30 @@
 
 void jack_bauer(void)
 {
-int minutes, hours;
-
-hours = 0;
-
-while (hours < 25)
+int hour1 = 0, hour2, minute1, minute2;
 {
-minutes = 0;
-
-while (minutes <= 59)
+while (hour1 < 3)
+hour2 = 0;
+while (hour2 <= ((hour1 == 2) ? 3 : 9))
 {
-_putchar(minutes + '0');
-minutes++;
-}
+minute1 = 0;
+while (minute1 < 6)
+{
+minute2 = 0;
+while (minute2 < 10)
+{
+_putchar(hour1 + '0');
+_putchar(hour2 + '0');
 _putchar(':');
-_putchar(hours + '0');
-hours++;
+_putchar(minute1 + '0');
+_putchar(minute2 + '0');
+_putchar('\n');
+minute2++;
+}
+minute1++;
+}
+hour2++;
+}
+hour1++;
 }
 }
