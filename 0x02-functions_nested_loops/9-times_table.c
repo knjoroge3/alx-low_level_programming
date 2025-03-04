7c27a89c618c8037693167012f592035d71924d6
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-04 21:34:22 +0300

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*times_table - Prints the 9 times table
+*
+*Description: Prints the 9 times table
+*
+*Return: void
+*/
+
+void times_table(void)
+{
+int a, b, c;
+
+int a = 0;
+
+while (a <= 9)
+{
+_putchar('0');
+_putchar('\n');
+}
+
+int b = 0;
+
+while (b < 9)
+{
+_putchar(b + '0');
+_Putchar(b + '2');
+b++;
+}
+}
+
+
