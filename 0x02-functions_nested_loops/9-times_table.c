38066f1a69779dac56890b2dde3e7f2a3ac28531
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-06 17:27:02 +0300

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,22 +12,46 @@ void times_table(void)
 {
 int a, b, c;
 
-int a = 0;
+a = 0;
 
 while (a <= 9)
 {
+if (a <= 8)
+{
 _putchar('0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('0');
+_putchar('$');
+_putchar(' ');
+}
+a++;
+}
 _putchar('\n');
+
+b = 0;
+
+while (b <= 9)
+{
+c = 0;
+if (c <= 9)
+{
+_putchar(c + '0');
+_putchar(',');
+_putchar(' ');
 }
 
-int b = 0;
-
-while (b < 9)
+else
 {
-_putchar(b + '0');
-_Putchar(b + '2');
+_putchar('0' + (c / 10));
+_putchar('0' + (c % 10));
+_putchar(',');
+_putchar(' ');
+}
 b++;
 }
+_putchar('\n');
 }
-
-
