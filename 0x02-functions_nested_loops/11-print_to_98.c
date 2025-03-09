d3b85c536b4940d7937dfc3fb5179663e2dab6eb
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-09 18:34:05 +0300

    98 Battery Street the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers passed
+ * to this function in order
+ *
+ * @n: integer to number 98;
+ *
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	while (n = 98)
+	{
+		if (n < 0)
+		{
+			_putchar((n * -1) + '0');
+		}
+		else if (n >=0)
+		{
+			_putchar(n + '0');
+		}
+		n++;
+
+		else if(n > 98)
+		{
+			_putchar(n + '0');
+		}
+		n--;
+	}
+	_putchar('\n');
+	return (0);
+}
+
