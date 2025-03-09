c78e61c308025d0ffb55f09b9f66cd4d6db600e7
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-09 18:10:44 +0300

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 /**
  * add - adds two integers
  *
- * Description: function that adds two integers
- * and resturns the result
+ * @int: integer one
+ * @int: integer two
  *
  * Return: the value of the added integer
  */
@@ -12,10 +12,5 @@
 int add(int, int)
 {
 	a, b;
-
-	int c;
-
-	c = (a + b);
-
-	return c;
+	return (a + b);
 }
