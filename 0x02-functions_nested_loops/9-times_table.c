45aedb862bb559a0a9654f80a95ba5cf386a74b1
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-09 17:12:37 +0300

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,17 +24,35 @@ void times_table(void)
 
 			if (c <= 9)
 			{
-				_putchar(c + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (b <= 8)
+				{
+
+					_putchar(c + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(c + '0');
+					_putchar('$');
+				}
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (b <=  8)
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					_putchar('$');
+				}
 			}
 			b++;
 		}
