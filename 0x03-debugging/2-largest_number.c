dd975fa6ddf8b257727a49bb185ef581e9822705
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-13 21:08:06 +0300

    0 >972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,7 +15,7 @@ int largest_number(int a, int b, int c)
 	if (a > b || a > c)
 	{
 	largest = a;
-	} 
+	}
 	else if (b > a || b > c)
 	{
 	largest = b;
@@ -23,7 +23,7 @@ int largest_number(int a, int b, int c)
 	else
 	{
 	largest = c;
-	}    
+	}
 
 	return (largest);
 }
