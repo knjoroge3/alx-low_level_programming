2a2bb8ea476eb0c1b050902fe189e3eeab23b3d4
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-15 15:58:53 +0300

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers btw 0 and 9
+ * Description: Prints numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
