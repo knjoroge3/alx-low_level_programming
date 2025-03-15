2d8be37c2c4745856df5d14e762857d5bcb44623
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-15 17:09:14 +0300

    numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers o to 14
+ * Description: these numbers are printed ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			{
+				if (j < 10)
+					_putchar(j + '0');
+				else
+				{
+					_putchar((j / 10) + '0');
+					_putchar((j % 10) + '0');
+				}
+
+			}
+		_putchar('\n');
+	}
+}
