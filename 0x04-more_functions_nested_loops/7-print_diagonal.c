fe5830f6e710680f42d607362b8d18bf6939a581
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-27 13:47:49 +0300

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,16 +3,33 @@
 /**
  * print_diagonal - prints diagonal
  * @n: The number of spaces to be printed before printing \
+ * @i: The number to increment spaces before the \
+ * @row: the row for printing backslashes
  *
  * Return: Void
  */
 
 void print_diagonal(int n)
 {
-	for (; n-- > 0; )
+	int i, row;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+
+	for (row = 0; row < n; row++)
+	{
+
+	for (i = 0; i < row; i++)
 	{
 		_putchar(' ');
 	}
+
 	_putchar('\\');
+
 	_putchar('\n');
+	}
 }
+
