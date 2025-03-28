d6bff4b76c68c53236ad2d0c08b19bdc6798b6f9
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-28 17:54:43 +0300

    How to get memory adress of a varibale. Note that this example does does compile with the flags -Wall -Wextra and -Werror as it has an error of expecting a integer return type while main has return type of void"

---
+++ 0x05-pointers_arrays_strings/adress.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	char c;
+	int n;
+
+	printf("The address of 'c' is: %p\n", &c);
+	printf("The address of 'n; is: %p\n", &n);
+
+	return (0);
+}
