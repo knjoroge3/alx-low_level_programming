3cb01b6ad7c962be8fcf9832e0898966a948ab00
knjoroge3 <ramadhanubuntu@gmail.com>
2025-03-31 19:52:32 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an integer and returns it value
+ *
+ *@n: The integer being returned
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
