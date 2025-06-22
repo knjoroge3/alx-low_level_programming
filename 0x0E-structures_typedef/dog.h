a5a06187605b30a311a595094aebd7f4a26c00fa
knjoroge3 <ramadhanubuntu@gmail.com>
2025-06-22 17:56:30 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - has three variables
+ *
+ * @name: 1st object
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: do we have any? dont think so
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
